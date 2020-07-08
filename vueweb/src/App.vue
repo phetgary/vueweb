284f97683b2f40d7bc9ccb6db87f1d3e950e05aa
phetgary <56030176+phetgary@users.noreply.github.com>
2020-07-08 18:10:18 +0800

    測試API以及載入套件

--- vueweb/src/App.vue
+++ vueweb/src/App.vue
@@ -2,22 +2,30 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  created() {
+    const api=`${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+    console.log(process.env.APIPATH, process.env.CUSTOMPATH);
+    this.$http.get(api).then((response) => {
+      console.log(response.data);
+    })
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='scss'>
+@import "./assets/all";
 </style>
