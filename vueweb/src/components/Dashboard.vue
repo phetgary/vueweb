78ff0d07b63c5bdc0a7897e4c10241fefb73574b
phetgary <56030176+phetgary@users.noreply.github.com>
2020-07-09 00:11:38 +0800

    新增dashboard頁面及路徑

---
+++ vueweb/src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div> 
+    <NavBar></NavBar>
+    <div class="container-fluid">
+      <div class="row">
+          <SideBar></SideBar>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+          <router-view></router-view>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SideBar from './SideBar';
+import NavBar from './NavBar';
+
+export default {
+  components:{
+    SideBar,
+    NavBar,
+  },
+};
+</script>
